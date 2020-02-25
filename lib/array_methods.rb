0e7a747a5113d6cc32e039362f0979c3f473ce49
Enrique Pittaluga <ep7m@me.com>
2020-02-25 02:27:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,12 +8,14 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  max = array[0]
   newarr = array.sort
   return newarr[-1]
   
 end
 
 def find_min_value(array)
+  min = array[0]
   newarr = array.sort
   return newarr[0]
 end
